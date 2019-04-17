4e2f42b0ad0cecc34d38adde471e834dbe08fb3c
Felipe Pereira <felipe@prodam.am.gov.br>
2019-04-17 18:18:36 -0400

    removing unnecessary files

--- authorization-server/src/main/resources/docker/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (username varchar(50) NOT NULL PRIMARY KEY, password varchar(200) NOT NULL, enabled boolean NOT null);
-CREATE TABLE authorities ( username varchar(50) NOT NULL, authority varchar(50) NOT NULL, CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username) );
-
-INSERT INTO users VALUES ('felipe.dspereira@gmail.com', '$2a$10$qsYSd6o1xDiulgzZxITEduAu1gmZBgNrxAoF9gdjrn2Df6PPp98Ey', true);
-INSERT INTO authorities VALUES ('felipe.dspereira@gmail.com', 'USER');
--- authorization-server/src/main/resources/docker/mysql.dockerfile
+++
@@ -1,3 +0,0 @@
-FROM mysql:8
-MAINTAINER Felipe Pereira <felipe.dspereira@gmail.com>
-ADD init.sql /docker-entrypoint-initdb.d
