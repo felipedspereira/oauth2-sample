66d5dc977bb5bdb6ed46fbdbc8da64e2233db884
Felipe Pereira <felipe@prodam.am.gov.br>
2019-04-30 10:53:13 -0400

    granting free access to the actuator/health url

--- authorization-server/src/main/java/com/felipe/authserver/authentication/WebSecurityConfig.java
+++ authorization-server/src/main/java/com/felipe/authserver/authentication/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,6 +23,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests()
+				.antMatchers("/actuator/health", "/login").permitAll()
+				.anyRequest().authenticated()
+			.and()
+				.formLogin();
+		// @formatter:on
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
